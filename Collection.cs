4a28fea4d52002f63d93335f5584f1266d8694ea
SuperPuper7565 <158828503+SuperPuper7565@users.noreply.github.com>
2024-03-12 17:41:14 +0500

    Add files via upload

---
+++ Collection.cs
@@ -0,0 +1,141 @@
+﻿using laba_10;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba_11
+{
+    public class TestCollections
+    {
+        public SortedSet<Shape> set1 = new SortedSet<Shape>();
+        SortedSet<string> set2 = new SortedSet<string>();
+        HashSet<Shape> set3 = new HashSet<Shape>();
+        HashSet<string> set4 = new HashSet<string>();
+        Rectangle first, last, middle, absent;
+        Stopwatch timer = new Stopwatch();
+        public TestCollections(int length)
+        {
+            while (set1.Count < length) 
+            {
+                Rectangle r = new Rectangle();
+                r.RandomInit();
+                if (!set1.Contains(r))
+                {
+                    set1.Add(r);
+                    set2.Add(r.ToString());
+                    set3.Add(r);
+                    set4.Add(r.ToString());
+                    if (set1.Count == 1)
+                        first = r.Clone() as Rectangle;
+                    if (set1.Count == length / 2)
+                        middle = r.Clone() as Rectangle;
+                    if (set1.Count == length)
+                        last = r.Clone() as Rectangle;
+                }
+            }
+        }
+        public void FindFirst()
+        {
+            Console.WriteLine("ПЕРВЫЙ ЭЛЕМЕНТ");
+            Console.WriteLine($"Коллекция 1, длина: {set1.Count}");
+            timer.Restart();
+            set1.Contains(first);
+            timer.Stop();
+            Console.WriteLine($"Объект найден, время поиска: {timer.ElapsedTicks} ");
+            Console.WriteLine($"Коллекция 2, длина: {set2.Count}");
+            timer.Restart();
+            set2.Contains(first.ToString());
+            timer.Stop();
+            Console.WriteLine($"Объект найден, время поиска: {timer.ElapsedTicks} ");
+            Console.WriteLine($"Коллекция 3, длина: {set3.Count}");
+            timer.Restart();
+            set3.Contains(first);
+            timer.Stop();
+            Console.WriteLine($"Объект найден, время поиска: {timer.ElapsedTicks} ");
+            Console.WriteLine($"Коллекция 4, длина: {set4.Count}");
+            timer.Restart();
+            set4.Contains(first.ToString());
+            timer.Stop();
+            Console.WriteLine($"Объект найден, время поиска: {timer.ElapsedTicks} ");
+        }
+        public void FindMiddle()
+        {
+
+            Console.WriteLine("СРЕДНИЙ ЭЛЕМЕНТ");
+            Console.WriteLine($"Коллекция 1, длина: {set1.Count}");
+            timer.Restart();
+            set1.Contains(middle);
+            timer.Stop();
+            Console.WriteLine($"Объект найден, время поиска: {timer.ElapsedTicks} ");
+            Console.WriteLine($"Коллекция 2, длина: {set2.Count}");
+            timer.Restart();
+            set2.Contains(middle.ToString());
+            timer.Stop();
+            Console.WriteLine($"Объект найден, время поиска: {timer.ElapsedTicks} ");
+            Console.WriteLine($"Коллекция 3, длина: {set3.Count}");
+            timer.Restart();
+            set3.Contains(middle);
+            timer.Stop();
+            Console.WriteLine($"Объект найден, время поиска: {timer.ElapsedTicks} ");
+            Console.WriteLine($"Коллекция 4, длина: {set4.Count}");
+            timer.Restart();
+            set4.Contains(middle.ToString());
+            timer.Stop();
+            Console.WriteLine($"Объект найден, время поиска: {timer.ElapsedTicks} ");
+        }
+        public void FindLast()
+        {
+
+            Console.WriteLine("ПОСЛЕДНИЙ ЭЛЕМЕНТ");
+            Console.WriteLine($"Коллекция 1, длина: {set1.Count}");
+            timer.Restart();
+            set1.Contains(last);
+            timer.Stop();
+            Console.WriteLine($"Объект найден, время поиска: {timer.ElapsedTicks} ");
+            Console.WriteLine($"Коллекция 2, длина: {set2.Count}");
+            timer.Restart();
+            set2.Contains(last.ToString());
+            timer.Stop();
+            Console.WriteLine($"Объект найден, время поиска: {timer.ElapsedTicks} ");
+            Console.WriteLine($"Коллекция 3, длина: {set3.Count}");
+            timer.Restart();
+            set3.Contains(last);
+            timer.Stop();
+            Console.WriteLine($"Объект найден, время поиска: {timer.ElapsedTicks} ");
+            Console.WriteLine($"Коллекция 4, длина: {set4.Count}");
+            timer.Restart();
+            set4.Contains(last.ToString());
+            timer.Stop();
+            Console.WriteLine($"Объект найден, время поиска: {timer.ElapsedTicks} ");
+        }
+        public void FindNot()
+        {
+            Rectangle r = new Rectangle();
+            Console.WriteLine("НЕСУЩЕСТВУЮЩИЙ ЭЛЕМЕНТ");
+            Console.WriteLine($"Коллекция 1, длина: {set1.Count}");
+            timer.Restart();
+            set1.Contains(r);
+            timer.Stop();
+            Console.WriteLine($"Объект не найден, время поиска: {timer.ElapsedTicks} ");
+            Console.WriteLine($"Коллекция 2, длина: {set2.Count}");
+            timer.Restart();
+            set2.Contains(r.ToString());
+            timer.Stop();
+            Console.WriteLine($"Объект не найден, время поиска: {timer.ElapsedTicks} ");
+            Console.WriteLine($"Коллекция 3, длина: {set3.Count}");
+            timer.Restart();
+            set3.Contains(r);
+            timer.Stop();
+            Console.WriteLine($"Объект не найден, время поиска: {timer.ElapsedTicks} ");
+            Console.WriteLine($"Коллекция 4, длина: {set4.Count}");
+            timer.Restart();
+            set4.Contains(r.ToString());
+            timer.Stop();
+            Console.WriteLine($"Объект не найден, время поиска: {timer.ElapsedTicks} ");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,284 @@
+﻿using laba_10;
+using System;
+using System.Collections;
+using System.Reflection.Metadata.Ecma335;
+using System.Diagnostics;
+namespace laba_11
+{
+    internal class Program
+    {
+        static Random random = new Random();
+        static void PrintStack(Stack stack)
+        {
+            foreach (Shape item in stack)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+        static void PrintSortedDictionary(SortedDictionary<Shape, Rectangle> sd)
+        {
+            foreach (var item in sd.Keys)
+            {
+                Console.WriteLine($"Ключ: {item}, значение: {sd[item]}");
+            }
+        }
+        static double GetDouble(string message, double min, double max)
+        {
+            bool isConvert;
+            double result;
+            do
+            {
+                Console.WriteLine(message);
+                isConvert = double.TryParse(Console.ReadLine(), out result);
+                if (!isConvert || result < min || result > max)
+                    Console.WriteLine("Ошибка ввода");
+            }
+            while (!isConvert || result < min || result > max);
+            return result;
+        }
+        static void AllSquareAreas(Stack shapes)
+        {
+            Console.WriteLine("Все площади квадратов");
+            int count = 0;
+            foreach (Shape shape in shapes)
+            {
+                if (shape is Rectangle r && !(shape is Parallelepiped))
+                {
+                    if (r.IsSquare())
+                    {
+                        Console.WriteLine(r.ToString()); ;
+                        count++;
+                    }
+                }
+            }
+            if (count == 0)
+                Console.WriteLine("Квадратов не обнаружено");
+        }
+        static void AllSquareAreas(SortedDictionary<Shape, Rectangle> sd)
+        {
+            Console.WriteLine("Все площади квадратов");
+            int count = 0;
+            foreach (var item in sd.Values)
+            {
+                if (item is Rectangle r && !(item is Parallelepiped))
+                {
+                    if (r.IsSquare())
+                    {
+                        Console.WriteLine(r.ToString()); ;
+                        count++;
+                    }
+                }
+            }
+            if (count == 0)
+                Console.WriteLine("Квадратов не обнаружено");
+        }
+        static void ShapesWithRightRadius(Stack shapes)
+        {
+            double radius = GetDouble("Введите радиус описанной окружности", 0, 100);
+            Console.WriteLine("Такой радиус описанной окружности у следующих прямоугольников:");
+            int count = 0;
+            foreach (Shape shape in shapes)
+            {
+                if (shape is Rectangle r && !(shape is Parallelepiped))
+                {
+                    if (Math.Pow(r.Width, 2) + Math.Pow(r.Length, 2) == Math.Pow(radius * 2, 2))
+                    {
+                        Console.WriteLine(r.ToString());
+                        count++;
+                    }
+                }
+            }
+            if (count == 0)
+                Console.WriteLine("подходящих прямоугольников не обнаружено");
+        }
+        static void ShapesWithRightRadius(SortedDictionary<Shape, Rectangle> sd)
+        {
+            double radius = GetDouble("Введите радиус описанной окружности", 0, 100);
+            Console.WriteLine("Такой радиус описанной окружности у следующих прямоугольников:");
+            int count = 0;
+            foreach (var item in sd.Values)
+            {
+                if (item is Rectangle r && !(item is Parallelepiped))
+                {
+                    if (Math.Pow(r.Width, 2) + Math.Pow(r.Length, 2) == Math.Pow(radius * 2, 2))
+                    {
+                        Console.WriteLine(r.ToString());
+                        count++;
+                    }
+                }
+            }
+            if (count == 0)
+                Console.WriteLine("подходящих прямоугольников не обнаружено");
+        }
+        static void ShapesWithRightArea(Stack shapes)
+        {
+            double area = GetDouble("Введите минимально возможную для фигуры площадь", 0, 10000);
+            int count = 0;
+            foreach (Shape shape in shapes)
+            {
+                if (shape is Rectangle && !(shape is Parallelepiped) || (shape is Circle))
+                {
+                    if (shape.GetArea() >= area)
+                    {
+                        Console.WriteLine(shape.ToString());
+                        count++;
+                    }
+                }
+            }
+            if (count == 0)
+                Console.WriteLine("Все фигуры меньше заданной площади");
+        }
+        static void ShapesWithRightArea(SortedDictionary<Shape, Rectangle> sd)
+        {
+            double area = GetDouble("Введите минимально возможную для фигуры площадь", 0, 10000);
+            int count = 0;
+            foreach (var shape in sd.Values)
+            {
+                if (shape is Rectangle && !(shape is Parallelepiped) || (shape is Circle))
+                {
+                    if (shape.GetArea() >= area)
+                    {
+                        Console.WriteLine(shape.ToString());
+                        count++;
+                    }
+                }
+            }
+            if (count == 0)
+                Console.WriteLine("Все фигуры меньше заданной площади");
+        }
+        static void Find (SortedDictionary<Shape, Rectangle> sd, Rectangle find)
+        {
+            bool ok = sd.ContainsKey(find);
+            if (ok)
+                Console.WriteLine($"Элемент {find} найден");
+            else
+                Console.WriteLine($"Элемент {find} не найден");
+        }
+        static Stack CloneStack(Stack other)
+        {
+            Stack clone = new Stack();
+            foreach (Shape item in other)
+            {
+                Shape item2 = new Shape(item);
+                clone.Push(item2);
+            }
+            return clone;
+        }
+        static SortedDictionary<Shape, Rectangle> CloneSD(SortedDictionary<Shape, Rectangle> other)
+        {
+            SortedDictionary<Shape, Rectangle> clone = new SortedDictionary<Shape, Rectangle> ();
+            foreach (Shape item in other.Keys)
+            {
+                Shape key = new Shape(item);
+                Rectangle value = new Rectangle(other[item]);
+                clone.Add(key, value);
+            }
+            return clone;
+        }
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Stack universal");
+            //Stack stack = new Stack();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    switch (random.Next(1, 4))
+            //    {
+            //        case 1:
+            //            {
+            //                Circle c = new Circle();
+            //                c.RandomInit();
+            //                stack.Push(c);
+            //                break;
+            //            }
+            //        case 2:
+            //            {
+            //                Rectangle r = new Rectangle();
+            //                r.RandomInit();
+            //                stack.Push(r);
+            //                break;
+            //            }
+            //        case 3:
+            //            {
+            //                Parallelepiped p = new Parallelepiped();
+            //                p.RandomInit();
+            //                stack.Push(p);
+            //                break;
+            //            }
+            //    }
+            //}
+            //PrintStack(stack);
+            //Console.WriteLine("Добавьте элемент");
+            //Circle c1 = new Circle();
+            //c1.Init();
+            //stack.Push(c1);
+            //PrintStack(stack);
+            //Console.WriteLine("Удалите элемент");
+            //Circle c2 = new Circle();
+            //c2.Init();
+            //Stack temp = new Stack();
+            //while (stack.Count > 0)
+            //{
+            //    Shape s = stack.Pop() as Shape;
+            //    if (!s.Equals(c2 as Shape))
+            //        temp.Push(s);
+            //}
+            //while (temp.Count > 0)
+            //{
+            //    Shape s = temp.Pop() as Shape;
+            //    stack.Push(s);
+            //}
+            //PrintStack(stack);
+            //AllSquareAreas(stack);
+            //ShapesWithRightArea(stack);
+            //ShapesWithRightRadius(stack);
+            //Stack stack2 = CloneStack(stack);
+            //stack2.Push(new Circle());
+            //PrintStack(stack2);
+            //Console.WriteLine();
+            //PrintStack(stack);
+            //Console.WriteLine("SortedDictionary<K,T>");
+            //SortedDictionary<Shape, Rectangle> sd = new SortedDictionary<Shape, Rectangle>();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    try
+            //    {
+            //        Rectangle value = new Rectangle();
+            //        value.RandomInit();
+            //        Shape key = value.GetBase;
+            //        sd.Add(key, value);
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        i--;
+            //    }
+            //}
+            //PrintSortedDictionary(sd);
+            //Console.WriteLine("Добавьте элемент");
+            //Rectangle r1 = new Rectangle();
+            //r1.Init();
+            //sd.Add(r1.GetBase, r1);
+            //PrintSortedDictionary(sd);
+            //Console.WriteLine("Удалите элемент");
+            //Rectangle r2 = new Rectangle();
+            //r2.Init();
+            //sd.Remove(r2.GetBase);
+            //PrintSortedDictionary(sd);
+            //Find(sd, r1);
+            //Find(sd, r2);
+            //AllSquareAreas(sd);
+            //ShapesWithRightArea(sd);
+            //ShapesWithRightRadius(sd);
+            //SortedDictionary<Shape, Rectangle> sd2 = CloneSD(sd);
+            //sd2.Add(r2.GetBase, r2);
+            //PrintSortedDictionary(sd2);
+            //Console.WriteLine();
+            //PrintSortedDictionary(sd);
+            //SortedSet<Shape> set1 = new SortedSet<Shape>();
+            //SortedSet<string> set2 = new SortedSet<string>();
+            //HashSet<Shape> set3 = new HashSet<Shape>();
+            //HashSet<string> set4 = new HashSet<string>();
+            TestCollections testCollections = new TestCollections(1000);
+            testCollections.FindNot();
+        }
+    }
+}
